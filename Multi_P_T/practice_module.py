0357b04d398a0e60d1c3a00c2f7c4b665c8c800e
JIHOJU <jihoju96@gmail.com>
2022-08-26 05:23:03 +0900

    docs: add multiprocessing & multithreading in python

---
+++ Multi_P_T/practice_module.py
@@ -0,0 +1,16 @@
+# IPython 에서 Multi-Processing 관련 함수 에러로 인한 모듈
+
+
+def is_prime(x):
+    if x <= 1:
+        return 0
+    elif x <= 3:
+        return x
+    elif x % 2 == 0 or x % 3 == 0:
+        return 0
+    i = 5
+    while i**2 <= x:
+        if x % i == 0 or x % (i + 2) == 0:
+            return 0
+        i += 6
+    return x
